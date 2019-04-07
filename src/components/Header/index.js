b34f539431073dd25425542c9682d7e2def0d898
Zkinsk <zoolunk@gmail.com>
2019-04-07 10:57:44 -0400

    additional page styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ import "./style.css";
 
 function Header(props) {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="navbar-brand"><h3>Click Game</h3></div>
+    <nav className="navbar navbar-light">
+      <div ><h3>Steven Universe</h3></div>
       <div className="scoreArea">
         <h4>Current&nbsp;Score:&nbsp;{props.currentScore}</h4>
         <h4>High&nbsp;Score:&nbsp;{props.highScore} </h4>
